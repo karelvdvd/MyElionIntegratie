50b7bb50f8f16f7e8c4b3283fccf93b8cb64af20
karelvdvd <karel.vdvd@gmail.com>
2026-06-04 16:35:12 +0200

    Update config_flow.py

--- custom_components/elion_integratie/config_flow.py
+++ custom_components/elion_integratie/config_flow.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 
 from .const import DOMAIN
 
@@ -19,9 +20,10 @@ class ElionConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         user_input: dict | None = None,
     ) -> config_entries.ConfigFlowResult:
         """Handle the initial step."""
+        errors: dict[str, str] = {}
 
         if user_input is not None:
-            await self.async_set_unique_id(user_input["username"])
+            await self.async_set_unique_id(user_input[CONF_USERNAME])
             self._abort_if_unique_id_configured()
 
             return self.async_create_entry(
@@ -31,13 +33,13 @@ class ElionConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         data_schema = vol.Schema(
             {
-                vol.Required("username"): str,
-                vol.Required("password"): str,
+                vol.Required(CONF_USERNAME): str,
+                vol.Required(CONF_PASSWORD): str,
             }
         )
 
         return self.async_show_form(
             step_id="user",
             data_schema=data_schema,
-            errors={},
+            errors=errors,
         )
